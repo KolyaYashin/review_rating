a9f7b6a3365cb1898c81dae964f1a22801d0196d
Yashin Nikolay <kol.yashin@yandex.ru>
2023-08-04 22:34:13 +0300

    final

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
 from flask import Flask,request,render_template, redirect
-
+from models import text_to_prediction
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def my_form():
-    return render_template('home.html',variable='Here will be comment',variable2='Here will be its rating')
+    return render_template('home.html',variable='Here will be comment',variable2='Here will be its rating',color_variable = 'azure')
 
 @app.route("/", methods=['POST'])
 def post():
     global text
     text = request.form['text']
-    processed_text = text.upper()+'ITSWORKINGFUCKYEAHH'
+    global rate
+    is_positive, rate = text_to_prediction(text)
+    global color_var
+    if is_positive:
+        color_var = 'green'
+    else:
+        color_var = 'red'
     return redirect('/comment')
+
+@app.route("/comment", methods=['POST'])
+def post_in_comment():
+    global text
+    text = request.form['text']
+    global rate
+    is_positive, rate = text_to_prediction(text)
+    global color_var
+    if is_positive:
+        color_var = 'green'
+    else:
+        color_var = 'red'
+    return redirect('/comment')
+
 
 @app.route('/comment')
 def comment():
-    return render_template('home.html',variable=text,variable2='CHECK')
-'''
-@app.route('/')
-def calculate_result():
-    a = str(request.args.get('val1'))
-    a=a+' ITS WORKING!!!'
-    return jsonify({"result":a})
-'''
+    return render_template('home.html',variable=text,variable2='Rating of this review is '+str(rate), color_variable = color_var)
 
 if __name__=='__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,19 +1,22 @@
 import pickle
 from keras.utils import pad_sequences
+from keras.models import load_model
 
 
-
+class_model = load_model('models/classification_reviews')
+model_regression = load_model('models/regression_reviews')
+max_tokens = 100
 
 
 def text_to_prediction(text:str):
-  with open('tokenizer.pickle', 'rb') as handle:
-    tokenizer = pickle.load(handle)
-  text = pad_sequences(tokenizer.texts_to_sequences([text]), maxlen=max_tokens, padding="post", truncating="post", value=0.)
-  pred = class_model(text)[0][0]
-  if pred>0.5:
-    is_positive = True
-    score = model_regression(text)[0][0]+8.5
-  else:
-    is_positive = False
-    score = model_regression(text)[0][0]+2.5
-  return is_positive, score
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+    text = pad_sequences(tokenizer.texts_to_sequences([text]), maxlen=max_tokens, padding="post", truncating="post", value=0.)
+    pred = 1-class_model(text)[0][0]
+    if pred>0.5:
+        is_positive = True
+        score = int(model_regression(text)[0][0]+8.5)
+    else:
+        is_positive = False
+        score = int(model_regression(text)[0][0]+2.5)
+    return is_positive, score
